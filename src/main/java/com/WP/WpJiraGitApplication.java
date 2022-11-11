a259423e40d07bad2f6129bc62d2bd60155f5131
rushibagul0909 <rushibagul0909@gmail.com>
2022-11-11 12:32:14 +0530

    project added

---
+++ src/main/java/com/WP/WpJiraGitApplication.java
@@ -0,0 +1,13 @@
+package com.WP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpJiraGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpJiraGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WP/WpJiraGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpJiraGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
